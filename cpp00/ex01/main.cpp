32177d9767f5860d038fd3f2f26e4704f2636fa7
otto-mata <ottomata@proton.me>
2025-03-28 18:29:29 +0100

    update telsoft header

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tblochet <tblochet@student.42.fr>                └─┘ ┴  ┴ └─┘        */
 /*                                                        ┌┬┐┌─┐┌┬┐┌─┐        */
 /*   Created: 2025/03/26 18:13:07 by tblochet             │││├─┤ │ ├─┤        */
-/*   Updated: 2025/03/28 14:29:13 by tblochet             ┴ ┴┴ ┴ ┴ ┴ ┴        */
+/*   Updated: 2025/03/28 18:27:38 by tblochet             ┴ ┴┴ ┴ ┴ ┴ ┴        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 int main()
 {
 	PhoneBook pb = PhoneBook();
+	std::cout << "T E L S O F T   S Y S T E M S" << std::endl;
+	std::cout << "      PhoneBook v5.3.0a" << std::endl;
 	while (pb.active())
 	{
 		pb.getUserCommand();
