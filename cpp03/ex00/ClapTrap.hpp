1e6b73fdf75315113ea01cd9b3a5a8352c757d4a
otto-mata <ottomata@proton.me>
2025-06-06 17:36:14 +0200

    refactor(cpp03): N/A

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -1,42 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                                            */
-/*   ClapTrap.hpp                                         ┌─┐┌┬┐┌┬┐┌─┐        */
-/*                                                        │ │ │  │ │ │        */
-/*   By: tblochet <tblochet@student.42.fr>                └─┘ ┴  ┴ └─┘        */
-/*                                                        ┌┬┐┌─┐┌┬┐┌─┐        */
-/*   Created: 2025/04/05 16:06:22 by tblochet             │││├─┤ │ ├─┤        */
-/*   Updated: 2025/04/05 16:31:59 by tblochet             ┴ ┴┴ ┴ ┴ ┴ ┴        */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tblochet <tblochet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/05 16:06:22 by tblochet          #+#    #+#             */
+/*   Updated: 2025/06/02 13:39:19 by tblochet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
-# define CLAPTRAP_HPP
-#include <string>
+#define CLAPTRAP_HPP
 #include <iostream>
+#include <string>
 
 class ClapTrap
 {
-	public:
-		/* ctor */	ClapTrap();
-		/* ctor */	ClapTrap(std::string ctName);
-		/* dtor */	~ClapTrap();
-		void		attack(const std::string &target);
-		void		takeDamage(unsigned int amount);
-		void		beRepaired(unsigned int amount);
-		int			hp() const;
-		void		hp(int newHp);
-		int			ep() const;
-		void		ep(int newEp);
-		int			atk() const;
-		void		atk(int newAttack);
-		std::string	name() const;
-		void		name(std::string &newName);
-	private:
-		int			_hp;
-		int			_ep;
-		int			_atk;
-		std::string	_name;
+public:
+  ClapTrap();
+  ClapTrap(std::string ctName);
+  ~ClapTrap();
+  void attack(const std::string& target);
+  void takeDamage(unsigned int amount);
+  void beRepaired(unsigned int amount);
+  int hp() const;
+  void hp(int newHp);
+  int ep() const;
+  void ep(int newEp);
+  int atk() const;
+  void atk(int newAttack);
+  std::string name() const;
+  void name(std::string& newName);
+
+private:
+  int _hp;
+  int _ep;
+  int _atk;
+  std::string _name;
 };
 
 #endif
--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -1,30 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                                            */
-/*   main.cpp                                             ┌─┐┌┬┐┌┬┐┌─┐        */
-/*                                                        │ │ │  │ │ │        */
-/*   By: tblochet <tblochet@student.42.fr>                └─┘ ┴  ┴ └─┘        */
-/*                                                        ┌┬┐┌─┐┌┬┐┌─┐        */
-/*   Created: 2025/04/05 16:05:51 by tblochet             │││├─┤ │ ├─┤        */
-/*   Updated: 2025/04/05 16:35:33 by tblochet             ┴ ┴┴ ┴ ┴ ┴ ┴        */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tblochet <tblochet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/05 16:05:51 by tblochet          #+#    #+#             */
+/*   Updated: 2025/06/02 13:39:35 by tblochet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-int main()
+int
+main()
 {
-	ClapTrap cl4ptp = ClapTrap();
-	ClapTrap shadow = ClapTrap("5H4D0W-TP");
-	
-	cl4ptp.attack(shadow.name());
-	shadow.takeDamage(cl4ptp.atk());
-	shadow.beRepaired(10);
-	shadow.atk(10);
-	shadow.attack(cl4ptp.name());
-	cl4ptp.takeDamage(shadow.atk());
-	shadow.attack(cl4ptp.name());
-	cl4ptp.takeDamage(shadow.atk());
-	shadow.attack(cl4ptp.name());
-	cl4ptp.takeDamage(shadow.atk());
+  ClapTrap cl4ptp = ClapTrap();
+  ClapTrap shadow = ClapTrap("5H4D0W-TP");
+
+  cl4ptp.attack(shadow.name());
+  shadow.takeDamage(cl4ptp.atk());
+  shadow.beRepaired(10);
+  shadow.atk(10);
+  shadow.attack(cl4ptp.name());
+  cl4ptp.takeDamage(shadow.atk());
+  shadow.attack(cl4ptp.name());
+  cl4ptp.takeDamage(shadow.atk());
+  shadow.attack(cl4ptp.name());
+  cl4ptp.takeDamage(shadow.atk());
 }
